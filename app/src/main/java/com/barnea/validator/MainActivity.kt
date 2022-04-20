7043fbdd71e6091db9dd154cfd8ff21662b36e76
Ido-Barnea <dodo.barnea@gmail.com>
2022-04-20 18:14:46 +0300

    - Changed example code

--- app/src/main/java/com/barnea/validator/MainActivity.kt
+++ app/src/main/java/com/barnea/validator/MainActivity.kt
@@ -4,22 +4,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Validator("My Text") // Create a Validator object with your text
-            .notEmpty() // Your Validations
-            .minLength(4, "Your text should be at least 4 characters long!")
+            .minLength(4, "Your text should be at least 4 characters long!") // Your Validations
+            .maxLength(10, "Your text can't be longer than 10 characters!")
             .addCallback(object: ValidatorCallback {
                 override fun onSuccess() {
-                    Toast.makeText(this@MainActivity, "Succeeded", Toast.LENGTH_SHORT).show()
+                    // Text validated successfully
                 }
 
                 override fun onFailure(error: String) {
-                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                    //Text did not pass validation tests
                 }
             })
             .validate() // Required, returns a boolean
